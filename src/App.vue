38297cfed797ef2cd7f61e4b02b86966fd6aaf58
Sajidul Kabir <sajidulkabiradib@gmail.com>
2022-07-02 19:55:58 +0600

    project setup

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="resource in resources" :key="resource.id">
+      {{ resource.title }}
+      {{ resource.description }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      resources: [
+        {
+          id: 1,
+          title: 'Official guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 2,
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
